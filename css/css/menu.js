9c5a7e80071d097304841b0525d8d15a01252ce7
Davirs10 <davirs10@hotmail.com>
2017-01-19 13:40:09 -0200

    Delete menu.js

--- css/css/menu.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() { 
-    var scroll = $(window).scrollTop();
- 
-    if (scroll > 800) {
-        $('.menu').addClass('menu-auxiliar');
-    } else {
-        $('.menu').removeClass('menu-auxiliar');
-    }
-});
